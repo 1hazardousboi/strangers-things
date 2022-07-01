b6cab64d8963f3e2b8c7236884751a42ec079254
1hazardousboi <sarinjthorn@gmail.com>
2022-07-01 13:32:51 -0600

    first changes

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export { default as Home } from "./Home";
+export { default as LogIn } from "./LogIn";
+export { default as Message } from "./Message";
+export { default as NavBar } from "./NavBar";
+export { default as NewPost } from "./NewPost";
+export { default as Profile } from "./Profile";
+export { default as SinglePost } from "./SinglePost";
